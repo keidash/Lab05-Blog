0446d990f7b447ce398c8b0e2301c4ed35f0dec0
keidash <94686371+keidash@users.noreply.github.com>
2023-02-23 12:51:03 +0000

    bodyParser

--- routes/articles.ts
+++ routes/articles.ts
@@ -1,4 +1,5 @@
 import Router, {RouterContext} from "koa-router";
+import bodyParser from "koa-bodyparser"
 
 const articles = [
   {titles: 'Hello articles', fullText: 'some text to fill the body'},
@@ -15,10 +16,23 @@ const getAll = async (ctx: RouterContext, next: any) => {
 }
 
 const createArticle = async (ctx: RouterContext, next: any) => {
+  let c: any = ctx.request.body;
+  let title = c.titles;
+  let fullText = c.fullText;
+  let newArticle = {title: title, fullText: fullText};
+  articles.push(newArticle);
+  ctx.status = 201;
+  ctx.body = newArticle;
   await next();
 }
 
 const getById = async (ctx: RouterContext, next: any) => {
+  let id = +ctx.params.id;
+  if((id < articles.length +1) && (id > 0)) {
+    ctx.body = articles[id-1];
+  } else {
+    ctx.status = 404;
+  }
   await next();
 }
 
@@ -31,8 +45,8 @@ const deleteArticle = async (ctx: RouterContext, next: any) => {
 }
 
 router.get('/', getAll);
-router.post('/', createArticle);
-router.get('/:id', getById);
+router.post('/', bodyParser(), createArticle);
+router.get('/:id([0-9]{1, })', getById);
 router.put('/:id', updateArticle);
 router.delete('/:id', deleteArticle);
 
